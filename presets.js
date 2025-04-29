cee534d1ce86d8179598af5c691511ffd0dad274
jammstudiosqatar <info@jammstudiosqatar.com>
2025-04-30 00:09:36 +0300

    Create presets.js

---
+++ presets.js
@@ -0,0 +1,8 @@
+// presets.js
+// Define your instrument-tuning presets here.
+// Key: display name; Value: array of note names (pitch classes + octave)
+window.tuningPresets = {
+  "6-String Standard": ["E2","A2","D3","G3","B3","E4"],
+  "6-String Drop D":   ["D2","A2","D3","G3","B3","E4"],
+  "4-String Bass":     ["E1","A1","D2","G2"]
+};
