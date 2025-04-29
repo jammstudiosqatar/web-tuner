936f840b860343fdc5d7310c7cbbc117f962f3c6
jammstudiosqatar <info@jammstudiosqatar.com>
2025-04-30 00:26:43 +0300

    Update presets.js

--- presets.js
+++ presets.js
@@ -1,6 +1,4 @@
 // presets.js
-// Define your instrument-tuning presets here.
-// Key: display name; Value: array of note names (pitch classes + octave)
 window.tuningPresets = {
   "6-String Standard": ["E2","A2","D3","G3","B3","E4"],
   "6-String Drop D":   ["D2","A2","D3","G3","B3","E4"],
